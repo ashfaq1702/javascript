33f2025a325534374ab0c9902b541675721968c3
DESKTOP-LS75C5U\ashfa <ashfaqadeni786@gmail.com>
2022-11-13 20:18:37 +0530

    variables in Javascript

---
+++ variables.js
@@ -0,0 +1,15 @@
+//javascript variables
+var num1=21
+var num2=22
+console.log(num1+num2)
+//data types
+var str="this is my string"
+console.log(str)
+
+//objects
+var marks={
+    ravi: 10,
+    ramesh:5,
+    harish:12
+}
+console.log(marks)
