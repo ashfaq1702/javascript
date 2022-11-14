21049199d901a80c836992e7eefb43486911627b
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-11-14 17:25:27 +0530

    operators in javascript

---
+++ operators.js
@@ -0,0 +1,28 @@
+//arithematic operators
+var a=10
+var b=20
+console.log(a+b,a-b,a*b,a%b)
+
+//assignment
+var x=10
+var y=x
+console.log(y)
+
+
+//COMPARITIVE 
+var x1=21;
+var x2=22;
+console.log(x1<x2,x1>x2,x1==x2)
+
+//logical
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
