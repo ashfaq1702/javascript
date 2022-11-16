bde81a811ed36e51cea9b853b19c4f48a7fe4b30
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-11-16 18:09:39 +0530

    constant and math

---
+++ math.js
@@ -0,0 +1,17 @@
+// a bunch of functions which help us to make changes with functions
+
+let x=3.1415
+x=Math.round(x) //round helps us to round off a decimal number 
+console.log(x)
+
+let y=5.95
+let z=-5.80
+y=Math.floor(y) // rounds down and returns the largest integer less than or equal to a given number.
+z=Math.floor(z)
+console.log(y)
+console.log(z)
+
+let a=0.95
+a=Math.ceil(a)
+ //function always rounds up and returns the smaller integer greater than or equal to a given number.
+console.log(a)
