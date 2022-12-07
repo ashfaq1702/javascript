cc33ba5e101f71958eaed5d8d0f56807d054cbbc
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-12-07 12:18:25 +0530

    Method chaining example

---
+++ MethodChaining.js
@@ -0,0 +1,4 @@
+// Method chaining is a process of writing one method after other to perform operations
+let myName='ashfaq'
+let updatedLetter= myName.charAt(0).toUpperCase();
+console.log(updatedLetter)
