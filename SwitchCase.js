c6e54f3ec73e64f9bffc06ea181f141d9746f2d6
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-12-09 15:52:13 +0530

    switch case

---
+++ SwitchCase.js
@@ -0,0 +1,24 @@
+//Switch case is an alternative of else-if ladder, it is much effecient than else-if ladder
+
+
+let grade='pizza'
+switch(grade){
+    case 'A':
+        console.log('Great')
+        break
+    case 'B':
+        console.log('Good')
+        break
+    case 'C':
+        console.log('okay')
+        break
+    case 'D':
+        console.log('barely passed')
+        break
+    case 'F':
+        console.log('failed!')
+        break
+    default:
+        console.log(grade,'not valid')
+        break
+}
