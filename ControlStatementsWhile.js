bc2d9eaa0a517f2e26081551bacf8a19716ed30f
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-12-09 16:43:20 +0530

    While loop

---
+++ ControlStatementsWhile.js
@@ -0,0 +1,14 @@
+// while loop repeats something untill the condition in it becomes false!
+let i=0
+while(i<100){
+    if(i==0){
+        console.log(0," is a whole number")
+    }
+    else if(i%2==0){
+        console.log(i,"is even")
+    }
+    else{
+        console.log(i,"is odd")
+    }
+    i=i+1
+}
