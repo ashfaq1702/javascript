9b4e9faec3d8e1bc0ce2bd381e8d2afc8e1b7db2
ashfaqadeni99 <ashfaqadeni786@gmail.com>
2022-12-21 12:42:35 +0530

    funtions

---
+++ funtions.js
@@ -0,0 +1,9 @@
+// Good for code reusability, life would've been tough for a programmer without funtions
+
+let age,name                                                         //declaring variables
+
+function happyBirthday(age, name){                                  //declaring a functon with arguments
+    console.log('happy', age,'birthday', name)
+}
+
+happyBirthday(24,'lexmon-laudamon')                                 //calling of a funtion
